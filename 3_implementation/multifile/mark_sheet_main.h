6a1490a5177fb13d793a477d9b926c23d5b5f3d7
karthick0403 <98890597+karthick0403@users.noreply.github.com>
2022-02-11 13:07:10 +0530

    Add files via upload

---
+++ 3_implementation/multifile/mark_sheet_main.h
@@ -0,0 +1,15 @@
+#ifndef __mark_sheet_main_H_
+#define __mark_sheet_main_H_
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<window.h>
+int m,m1,n,n1,o,o1,p,p1,q1,q2,r,total,per,t1,t2,t3,t4,t5;
+char b[20],ch[20],a[50],e[8],d[30],grade,res[5],rem[16],q,x,y,u,v;
+char s[] = "pass";
+char s1[] = "fail";
+char s3[] = "Congratulations";
+char s4[] = "     ";
+
+#endif
