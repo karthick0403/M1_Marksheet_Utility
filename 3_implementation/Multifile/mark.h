a982cd560f936c97dede413e3fba6ba13dc70458
karthick0403 <98890597+karthick0403@users.noreply.github.com>
2022-02-17 10:21:34 -0500

    Add files via upload

---
+++ 3_implementation/Multifile/mark.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+ void input();
+ int calculations();
+ void  karthi1();
+ void   karthi2();
+ void  karthi3();
+ void   karthi4();
+ void  karthi5();
+ void resultfunction();
+ void remarkfunction();
+ void show();
