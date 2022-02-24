49debc745d39e57b68abff850df1c5fe058005d3
karthick0403 <98890597+karthick0403@users.noreply.github.com>
2022-02-24 15:05:04 +0530

    Add files via upload

---
+++ 3_implementation/src/markmain.c
@@ -0,0 +1,15 @@
+#include"mark.h"
+int main(){
+	input();
+	calculations();
+	 karthi1();
+     karthi2();
+     karthi3();
+     karthi4();
+     karthi5();
+	resultfunction();
+	remarkfunction();
+	show();
+	return 0;
+	
+}
